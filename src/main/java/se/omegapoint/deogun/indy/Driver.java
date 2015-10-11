54a35f4402db0d34206180633164b580805d28f4
Daniel Deogun <daniel@deogun.se>
2015-10-11 10:07:14 +0200

    Simplified Driver code to illustrate constant linkage

--- src/main/java/se/omegapoint/deogun/indy/Driver.java
+++ src/main/java/se/omegapoint/deogun/indy/Driver.java
@@ -1,20 +1,16 @@
 package se.omegapoint.deogun.indy;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static java.lang.invoke.MethodType.methodType;
 
 public class Driver {
     public static void main(String[] args) throws Throwable {
-        final MethodType methodType = MethodType.methodType(void.class);
+        final MethodHandle indy = lookup().findVirtual(ClassWithInvokedynamic.class, "indy", methodType(void.class));
 
-        final MethodHandles.Lookup lookup = MethodHandles.lookup();
-
-        final MethodHandle methodHandle = lookup.findVirtual(ClassWithInvokedynamic.class, "indy", methodType);
-
-        final ClassWithInvokedynamic invoker = new ClassWithInvokedynamic();
-        methodHandle.invokeExact(invoker);
-        methodHandle.invokeExact(invoker);
-        methodHandle.invokeExact(invoker);
+        indy.invokeExact(new ClassWithInvokedynamic());
+        indy.invokeExact(new ClassWithInvokedynamic());
+        indy.invokeExact(new ClassWithInvokedynamic());
     }
 }
