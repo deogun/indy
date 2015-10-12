f3337c37a30f56f5341cba8c24eb18358b2368fd
Daniel Deogun <daniel@deogun.se>
2015-10-12 22:40:58 +0200

    added example of stateless and stateful lambdas

--- src/main/java/se/omegapoint/deogun/bytecode/Evaluation.java
+++ src/main/java/se/omegapoint/deogun/bytecode/Evaluation.java
@@ -1,13 +1,25 @@
 package se.omegapoint.deogun.bytecode;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 import static java.util.Arrays.asList;
 
 public class Evaluation {
-    public static void main(String[] args) {
+    int factor = 100;
+
+    public void foo() {
         final List<Integer> values = asList(1, 2, 3, 4, 5);
 
-        System.out.println(values.stream().reduce(0, (a, b) -> a + b));
+        System.out.println(values.stream().reduce(0, stateful()));
+        System.out.println(values.stream().reduce(0, stateless()));
+    }
+
+    private BinaryOperator<Integer> stateful() {
+        return (a, b) -> a + b + factor;
+    }
+
+    private BinaryOperator<Integer> stateless() {
+        return (a, b) -> a + b;
     }
 }
